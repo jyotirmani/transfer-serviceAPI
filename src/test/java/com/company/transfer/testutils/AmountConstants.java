907777166f724403631177393f44283c28f82e4d
Emiliano Vaz Fraga <emivaf@br.ibm.com>
2018-02-26 20:25:25 -0300

    Add new BigDecimal constants to AmountConstants

--- src/test/java/com/company/transfer/testutils/AmountConstants.java
+++ src/test/java/com/company/transfer/testutils/AmountConstants.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 public class AmountConstants {
 	public static final BigDecimal AMOUNT_NEGATIVE_ONE = new BigDecimal(-1);
 	public static final BigDecimal AMOUNT_10 = new BigDecimal("10");
+	public static final BigDecimal AMOUNT_50 = new BigDecimal("50");
 	public static final BigDecimal AMOUNT_100 = new BigDecimal("100");
 	public static final BigDecimal AMOUNT_1000 = new BigDecimal("1000");
 	public static final BigDecimal AMOUNT_1000000 = new BigDecimal("1000000");
